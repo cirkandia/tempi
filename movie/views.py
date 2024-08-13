14b66704a467c012c7341b5e35d34e4e115914b4
Cirkandia <jemesav@eafit.edu.co>
2024-08-12 23:09:11 -0500

    "full pagina ready"

--- movie/views.py
+++ movie/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 # Create your views here.
 def home(request):
     #return render(request, 'home.html')
+
+
+    searchterm = request.GET.get('searchMovie')
+    if searchterm:
+        movies=Movie.objects.filter(title__icontains=searchterm)
+    else:
+        movies=Movie.objects.all()
+    return render(request, 'home.html', {'searchterm':searchterm, 'movies':movies})
+
     return render(request, 'home.html', {'name': 'johan mesa'})
+
+    
+
 
 def About(request):
     return HttpResponse('<h1>about aqui???</h1>')
